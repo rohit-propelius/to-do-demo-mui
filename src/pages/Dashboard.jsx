c516c4a9fdf5062c2c6c83c0a96bdbc357932d72
pratik <pratik.kapadia@propelius.tech>
2023-03-20 13:00:44 +0530

    added basic sidebar/drawer using mui

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-  
+import { Box, Typography, AppBar, Toolbar, IconButton, Drawer } from '@mui/material';
+import { Apps, Menu, ContactMail, AssignmentInd, Home } from "@mui/icons-material";
+import React, {useState} from 'react';
 function Dashboard (){
- return (
-    <div>
-        This is Dashboard
-    </div>
- )
+
+    const [open, setOpen] = useState(false);
+
+    const toggleSlider = () => {
+        setOpen(!open);
+    };
+    return (
+        <Box 
+            backgroundColor={'black'}
+            width={'100vw'}
+            height={'100vh'}
+        >
+            <AppBar position={'static'}>
+                <Toolbar>
+                    <IconButton onClick={toggleSlider}>
+                        <Menu/>
+                    </IconButton>
+                    <Typography color={'white'}>
+                        To Do List
+                    </Typography>
+                    <Drawer open={open} anchor={'left'} onClose={toggleSlider} width={'33.33%'}>
+                        this is list
+                    </Drawer>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
-  
+
 export default Dashboard;
